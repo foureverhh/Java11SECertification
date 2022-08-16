782f910fca2693a175ffc4b0b9b8a305ec648cbd
Yuanheng Li <Yuanheng.Li@skolverket.se>
2022-08-16 10:55:47 +0200

    Fields and variable

---
+++ DataTypesAndStringAPIs/src/FieldsAndVariables.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class FieldsAndVariables {
+    public static void main(String[] args) {
+        boolean flag = (new Random()).nextBoolean();
+        long variable_x;
+        if (flag) {
+            variable_x = 100L;
+        } else {
+            variable_x = 500L;
+        }
+        System.out.println("variable_x is " + variable_x);
+
+        System.out.println("Primitive integers");
+        byte maxByte = 127; //8bits
+        byte minByte = -128;
+
+        short maxShort = 32767; //16bits
+        short minShort = -32768;
+
+        int maxInt = 2147483647; //32bits
+        int minInt = -2147483648;
+
+        long maxLong = 9223372036854775807L; //64bits
+        long minLong = -9223372036854775808L;
+
+        System.out.println("Primitive decimals");
+        float piFloat = 3.14f; //32 bit
+        double piDouble = 3.14; //64 bit
+
+
+
+
+    }
+}
+
