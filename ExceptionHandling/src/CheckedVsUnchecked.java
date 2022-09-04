65355130af9e9fd6e32c5a969f38518a75c91acf
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-09-04 19:16:05 +0200

    unchecked and checked exception

---
+++ ExceptionHandling/src/CheckedVsUnchecked.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class CheckedVsUnchecked {
+    public static void main(String[] args) {
+        System.out.println("unchecked exception -> runtimeException, checked exception -> IOException");
+        compileCompilesRuntimeException();
+        try {
+            compilerDoNotCompileIOException(); //IOException do not
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("This will not run");
+    }
+
+    private static void compilerDoNotCompileIOException() throws IOException{
+        throw new IOException();
+    }
+
+    private static void compileCompilesRuntimeException() {
+        throw new IllegalArgumentException();
+    }
+}
