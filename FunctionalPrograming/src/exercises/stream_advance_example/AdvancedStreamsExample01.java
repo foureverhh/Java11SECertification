dac341e415263597dd2a16734c0c201ed8d996c2
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-09-06 23:48:07 +0200

    Stream.generate, Stream.iterate(seed, predict ,unary)

---
+++ FunctionalPrograming/src/exercises/stream_advance_example/AdvancedStreamsExample01.java
@@ -0,0 +1,18 @@
+package exercises.stream_advance_example;
+
+import java.math.BigInteger;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class AdvancedStreamsExample01 {
+    public static void main(String[] args) {
+        Stream<UUID> uuids = Stream.generate(UUID::randomUUID).limit(10);
+        uuids.forEach(System.out::println);
+
+        Stream<BigInteger> powerOfTwo = Stream.iterate(BigInteger.ONE, num -> num.multiply(BigInteger.TWO));
+        powerOfTwo.limit(20).forEach(System.out::println);
+
+        Stream<Character> characterStream = Stream.iterate('A', letter -> letter.compareTo('Z') <= 0, letter -> (char) (letter + 1));
+        characterStream.forEach(System.out::print);
+    }
+}
