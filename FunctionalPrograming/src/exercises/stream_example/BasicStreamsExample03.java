c8062b90c55e33e603fcb27d65f257201f19d912
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-09-08 22:08:29 +0200

    review BasicStreamExample03 flatMAp

--- FunctionalPrograming/src/exercises/stream_example/BasicStreamsExample03.java
+++ FunctionalPrograming/src/exercises/stream_example/BasicStreamsExample03.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 
 public class BasicStreamsExample03 {
     public static void main(String[] args) {
+        System.out.println("flatMap map");
         List<Product> products = MockDataSource.getProducts();
         products.stream()
                 .filter(product -> product.getCategory() == Category.FOOD)
@@ -24,10 +25,15 @@ public class BasicStreamsExample03 {
                 .flatMap(product -> spaces.splitAsStream(product.getName()))
                 .forEach(System.out::println);
         System.out.println("*********************************");
-        System.out.println("map is for one to one, flatMap is for one to many");
+
+        System.out.println("map is for one to one, flatMap is for one to many, map convert an array of string element to ONE object as Stream<Stream<String>>, flatMap convert the same array element direct to Stream<String>");
+        System.out.println("*************streamWithMap*****************");
         Stream<Stream<String>> streamWithMap = products.stream()
                 .map(product -> spaces.splitAsStream(product.getName()));
+        streamWithMap.forEach(System.out::println);
+        System.out.println("*************streamWithFlatMap*****************");
         Stream<String> streamWithFlatMap = products.stream()
                 .flatMap(product -> spaces.splitAsStream(product.getName()));
+        streamWithFlatMap.forEach(System.out::println);
     }
 }
