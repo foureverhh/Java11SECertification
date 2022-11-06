80dfdd920816b28c6617e9b1b32a10f5baa5e965
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-11-06 21:42:59 +0100

    list add(index, item) to insert one item

--- java-collections/src/com/pluralsight/ListMain.java
+++ java-collections/src/com/pluralsight/ListMain.java
@@ -1,2 +1,23 @@
-package com.pluralsight;public class ListMain {
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ListMain {
+    public static void main(String... args) {
+        ArrayList arrayList1 = new ArrayList();
+        List arrayList2 = new ArrayList();
+        Collection arrayList3 = new ArrayList();
+
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("yellow");
+        colors.add("blue");
+        System.out.println(colors);
+        System.out.println("----------");
+        colors.add(1,"orange");
+        System.out.println(colors);
+    }
 }
