83b46c7022295affd998add4b9ff9e31330d541f
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-11-09 21:54:27 +0100

    synchronized on a class instance

--- concurrency-1z0-819/concurrency-1z0-819/src/synchronize_lock/Count.java
+++ concurrency-1z0-819/concurrency-1z0-819/src/synchronize_lock/Count.java
@@ -13,10 +13,27 @@ public class Count {
                     " with id " + Thread.currentThread().getId());
         }
     }
+
+    // lock on an instance
+    public void incrementCounterInstanceMethod() {
+        synchronized (this) {
+            int current = counter;
+            System.out.println("Instance lock before: " + counter + " current thread: " + Thread.currentThread().getName() +
+                    " with id " + Thread.currentThread().getId());
+            counter = current + 1;
+            System.out.println("Instance lock after: " + counter + " current thread: " + Thread.currentThread().getName() +
+                    " with id " + Thread.currentThread().getId());
+        }
+    }
     public static void main(String[] args) throws InterruptedException {
+        Count count = new Count();
         for (int i = 0; i < 10; i++) {
             Thread t = new Thread(() -> {
-                incrementCounter();;
+                // lock on object lock
+                incrementCounter();
+                // lock on instance
+                count.incrementCounterInstanceMethod();
+                System.out.println("------");
             }, "t" + i);
             t.start();
         }
