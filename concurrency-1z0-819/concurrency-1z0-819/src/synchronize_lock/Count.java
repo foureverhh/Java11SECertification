0d466556340543f9267ac9f7d8249d0376b44418
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-11-09 21:40:33 +0100

    synchronized on a final object

--- concurrency-1z0-819/concurrency-1z0-819/src/synchronize_lock/Count.java
+++ concurrency-1z0-819/concurrency-1z0-819/src/synchronize_lock/Count.java
@@ -2,15 +2,17 @@ package synchronize_lock;
 
 public class Count {
     public static int counter;
-    public static synchronized void incrementCounter() {
-        int current = counter;
-        System.out.println("Before: " + counter + " current thread: " + Thread.currentThread().getName() +
-                " with id " + Thread.currentThread().getId());
-        counter = current + 1;
-        System.out.println("After: " + counter + " current thread: " + Thread.currentThread().getName() +
-                " with id " + Thread.currentThread().getId());
+    private static final Object lock = new Object();
+    public static void incrementCounter() {
+        synchronized (lock) {
+            int current = counter;
+            System.out.println("Before: " + counter + " current thread: " + Thread.currentThread().getName() +
+                    " with id " + Thread.currentThread().getId());
+            counter = current + 1;
+            System.out.println("After: " + counter + " current thread: " + Thread.currentThread().getName() +
+                    " with id " + Thread.currentThread().getId());
+        }
     }
-
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             Thread t = new Thread(() -> {
