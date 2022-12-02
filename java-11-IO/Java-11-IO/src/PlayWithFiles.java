eddd4235fbe44f442cd78844e4a781a740cce660
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-12-02 21:51:53 +0100

    java.io File java.nio Path

--- java-11-IO/Java-11-IO/src/PlayWithFiles.java
+++ java-11-IO/Java-11-IO/src/PlayWithFiles.java
@@ -27,12 +27,31 @@ public class PlayWithFiles {
             System.out.println("dir1 exists = " + dirs1.exists());
             //mkdir() creates only on directory
             //mkdirs() creates multi layer dir
-            System.out.println("dirs1 is deleted = " + dirs1.delete());
-
+            System.out.println("dirs1 is delete() = " + dirs1.delete());
+            dirs1.deleteOnExit();
+            System.out.println("dirs1 exists after deleteOnExit() = " + dirs1.exists());
+            System.out.println("dirs1 = " + dirs1);
             String name = dirs1.getName();
             String parent = dirs1.getParent();
             String path = dirs1.getPath();
 
+            System.out.println("name = " + name);
+            System.out.println("parent = " + parent);
+            System.out.println("path = " + path);
+
+            dir = new File("images/debug.png");
+            name = dir.getName();
+            parent = dir.getParent();
+            path = dir.getPath();
+            System.out.println("name = " + name);
+            System.out.println("parent = " + parent);
+            System.out.println("path = " + path);
+
+            dir = new File("images/images/debug.png");
+            String absolutePath = dir.getAbsolutePath();
+            String canonicalPath = dir.getCanonicalPath();
+            System.out.println("absolutePath = " + absolutePath);
+            System.out.println("canonicalPath = " + canonicalPath);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
---
+++ java-11-IO/Java-11-IO/src/PlayWithPath.java
@@ -0,0 +1,99 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PlayWithPath {
+    public static void main(String[] args) {
+        // Path path1 = Paths.get("images/seaside.jpg");
+        Path path1 = Paths.get("/images/seaside.jpg");
+        Path path2 = Path.of("images/mountain.jpg");
+        System.out.println("path1 = " + path1);
+        System.out.println("path2 = " + path2);
+        System.out.println("path1 = " + path1.getClass());
+        System.out.println("path2 = " + path2.getClass());
+
+        boolean absolute = path1.isAbsolute();
+        System.out.println("absolute = " + absolute);
+
+        Path root1 = path1.getRoot();
+        System.out.println("root1 = " + root1);
+        Path root2 = path2.getRoot();
+        System.out.println("root2 = " + root2);
+
+        //resolve
+
+        //resolve with empty or relative path, cancat
+        //resolve with absolute path, only takes absolute path
+        Path pathAbsolute1 = Path.of("C:/myImages/sunrise.jpg");
+        Path pathRelative1 = Path.of("sunset.jpg");
+
+        Path resolve = pathAbsolute1.resolve(pathRelative1);
+        System.out.println("pathAbsolute resolves pathRelative = " + resolve);
+        //result /images/sunrise.jpg/sunset.jpg
+
+        Path pathRelative2 = Path.of("images/sunrise.jpg");
+        resolve = pathRelative2.resolve(pathRelative1);
+        System.out.println("Relative resolve relative: " + resolve); // result images/sunrise.jpg/sunset.jpg
+
+        Path placeHolder = Path.of("");
+        resolve = pathAbsolute1.resolve(placeHolder);
+        System.out.println(resolve); // /images/sunrise.jpg
+        resolve = pathRelative2.resolve(placeHolder);
+        System.out.println(resolve); // images/sunrise.jpg
+
+        Path pathAbsolute2 = Path.of("/images/hello.jpg");
+        resolve = pathAbsolute1.resolve(pathAbsolute2);
+        System.out.println("absolut resolve absolute: " + resolve); // /images/hello.jpg
+        resolve = pathAbsolute1.resolve(pathRelative1);
+        System.out.println(resolve);
+
+
+        // test resolveSibling()
+        // replace file
+        path1 = Paths.get("c:/images/mountain.jpg");
+        path2 = Path.of("seaside.jpg");
+        Path resolveSibling = path1.resolveSibling(path2);
+        System.out.println("resolveSibling = " + resolveSibling); // c:/images/seaside.jpg
+        path1 = Paths.get("mountain.jpg");
+        resolveSibling = path1.resolveSibling(path2);
+        System.out.println("resolveSibling with this path as relative path, or the other is absolute, then only take the other: " + resolveSibling );
+        // seaside.jpg
+
+        path1 = Paths.get("c:/images/mountain.jpg");;
+        path2 = Path.of("");
+        System.out.println("absolute resolve empty, returns the parent of this parent: " + path1.resolveSibling(path2));
+        // c:/images
+
+
+        // relativize()
+        // find relative path from this path to other path
+        System.out.println("relativize() testing");
+        path1 = Paths.get("C:/images/jpgs/");
+        path2 = Paths.get("C:/images/pngs/");
+        System.out.println("path1: " + path1);
+        System.out.println("path2: " + path2);
+        Path relativize = path1.relativize(path2);
+        System.out.println("relativize absolute with absolute: " + relativize); // ../pngs
+        path1 = Paths.get("C:/videos/");
+        relativize = path1.relativize(path2);
+        System.out.println("relativize absolute with absolute: " + relativize); // ../images/pngs
+
+        // normalize()
+        // remove redundancies from a path
+        System.out.println("normalize()");
+        path1 = Path.of("videos/../videos/../videos");
+        path2 = Path.of("images/pngs/");
+        Path normalize = path1.normalize();
+        System.out.println("normalize: " + normalize);
+        normalize = path2.normalize();
+        System.out.println("normalize: " + normalize);
+
+
+        /**
+         * File fs is representative the platform's local file system
+         * FileSystem fs = DefaultFileSystem.getFileSystem();
+         *
+         * FilesSystems.getDefault().getPath();
+         */
+
+    }
+}
